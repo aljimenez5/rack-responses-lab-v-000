6f25320db2fed476c06381d254c319ead91aba76
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 01:59:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,12 @@ require 'time'
 class Application
   
   def call(env)
-  resp = Rack::Response.new
-  twelve_afternoon = Time.at(1234).strftime "%H:%M"
-  if Time.now.strftime "%H:%M".to_i == (12..23)
-    resp.write "Good Afternoon!"
-  else 
-    resp.write "Good Morning!"
+    resp = Rack::Response.new
+    twelve_afternoon = Time.at(1234).strftime "%H:%M"
+    if Time.now.strftime "%H:%M".to_i == (12..23)
+      resp.write "Good Afternoon!"
+    else 
+      resp.write "Good Morning!"
+    end
   end
-  
 end
