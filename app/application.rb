52b0f7ee64301b947ece42e75c284832c89d5116
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 01:59:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,6 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    twelve_afternoon = Time.at(1234).strftime "%H:%M"
     if Time.now.strftime "%H:%M".to_i == (12..23)
       resp.write "Good Afternoon!"
     else 
