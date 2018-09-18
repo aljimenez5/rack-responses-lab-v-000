120675eea281449da96b98f4facfe30de7299b12
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 02:00:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     current_time = Time.now.strftime "%H:%M"
-    if .to_i == (12..23)
+    if current_time.to_i == (12..23)
       resp.write "Good Afternoon!"
     else 
       resp.write "Good Morning!"
