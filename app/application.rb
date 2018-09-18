ce6744354e1aabf1eab1b578928f38793d064508
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 01:58:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,8 @@ class Application
   def call(env)
   resp = Rack::Response.new
   twelve_afternoon = Time.at(1234).strftime "%H:%M"
-  if Time.now < Time.new()
+  if Time.now.strftime "%H:%M".to_i == (12..23)
+    resp.write
   
   resp.write
   
