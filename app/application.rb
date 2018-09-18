d0cc763bb7a9f5239c7e4503b7fdbf77cb479247
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-18 02:03:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    if Time.now.hour, (12..23)
+    if Time.now.hour.between?(12,23)
       resp.write "Good Afternoon!"
     else 
       resp.write "Good Morning!"
